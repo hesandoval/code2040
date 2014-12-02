5ddb51733f12aadf343650f118b3e89746aa4f48
Hector Sandoval <hesandova@gmail.com>
2014-12-01 21:30:40 -0800

    Changed params keyword arg to data

--- apikeygen.py
+++ apikeygen.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-payload = {'email': 'hsandoval@csumb.edu', 'github': 'https://github.com/hesandoval/code2040'}
+payload = {'email': 'hsandoval@csumb.edu', 'github': 'https://github.com/hesandoval/code2040.git'}
 payload = json.dumps(payload)
 print payload
-r = requests.post('http://challenge.code2040.org/api/register', params=payload)
+r = requests.post('http://challenge.code2040.org/api/register', data=payload)
 
 print r.text
