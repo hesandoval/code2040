292794e66900d8558e8e4f939e8b15a3b58a6e6f
Hector Sandoval <hesandova@gmail.com>
2014-12-04 21:34:27 -0800

    Added reversestring.py file

---
+++ reverseString.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+apiKey = 'OOR23GuWwf'
+payload = {'token':apiKey}
+payload = json.dumps(payload)
+
+r = requests.post('http://challenge.code2040.org/api/getstring',data=payload)
+print json.loads(r.text)['result']
+
+stringToReverse = json.loads(r.text)['result']
+# stringToReverse = 'jason'
+reversedString = ''
+i = len(stringToReverse) - 1
+while(i >= 0):
+    reversedString = reversedString + stringToReverse[i]
+    i -= 1
+
+r = requests.post('http://challenge.code2040.org/api/validatestring',
+                  json.dumps({"token": apiKey, "string": reversedString}))
+print r.text
